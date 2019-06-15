892becd9320e41af3e93c818d0689bfc9528dc95
beejay1293 <mobolajimatti@Mobolajis-MacBook-Pro.local>
2019-06-15 17:44:26 +0100

    ch(set-up-webpack-for-react): webpack set-up
    - install webpack, webpack-cli, webpack-dev-server
    - install react-hot-loader
    - add .gitignore file
    - add node_modules to .gitignore
    - add src/index.js
    - add dist/index.html
    - add webpack.config.js
    - add start script to package.json
    
    [Delievers #166698554]

---
+++ src/index.js
@@ -0,0 +1,14 @@
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+    <div>
+       
+    </div>,
+     document.getElementById('app')
+   );
+
+module.hot.accept();
